9211618ecf22cc45e975788f443bae69078a7f38
EC2 Default User <ec2-user@ip-172-31-13-114.ap-northeast-1.compute.internal>
2022-11-08 12:45:16 +0000

    開発準備完了

---
+++ db/migrate/20221108114325_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+      
+      t.string :name
+      t.text :introduction
+      t.integer :price
+      t.boolean :is_active, default: true
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20221108114536_create_orders.rb
@@ -0,0 +1,15 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      
+      t.string :postal_code
+      t.string :address_code
+      t.string :name
+      t.integer :postage
+      t.integer :bill
+      t.integer :payment_method
+      t.integer :status
+      t.timestamps
+    end
+  end
+end
