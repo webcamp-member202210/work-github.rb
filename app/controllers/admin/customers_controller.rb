74a0138beedcf1ea6b3957d4c89eddf9807014ef
EC2 Default User <ec2-user@ip-172-31-13-114.ap-northeast-1.compute.internal>
2022-11-18 13:55:46 +0000

    [Update] レイアウト変更更新

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,15 +1,23 @@
 class Admin::CustomersController < ApplicationController
-  
+
   def index
-    @customers = Customer.all
+    @customers = Customer.page(params[:page])
   end
-  
+
   def show
-    @customer = customer.find(params[:id])
-  end  
-  
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+  end
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
   end
