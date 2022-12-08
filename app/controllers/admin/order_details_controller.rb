c11dc0dd43354725f22484812d3d55ccce072727
EC2 Default User <ec2-user@ip-172-31-13-114.ap-northeast-1.compute.internal>
2022-12-08 13:00:38 +0000

    [Update]テスト実施中2

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,7 +1,17 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @count = 0
     if @order_detail.update(order_detail_params)
+      @order.order_details.each do |order_detail|
+        if order_detail.making_status == "complete"
+          @count += 1
+        end
+      end
+      if @order.order_details.count == @count
+        @order.update(status: 3)
+      end
       flash[:notice] = "製造ステータスを更新しました"
       redirect_to admin_order_path(@order_detail.order.id)
     else
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,21 +2,27 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
-  
+
   def update
     @order = Order.find(params[:id])
     if @order.update(order_params)
+      if @order.status == "confirm"
+        @order.order_details.each do |order_detail|
+          order_detail.update(making_status: 1)
+        end
+      end
       flash[:notice] = "注文ステータスを更新しました"
       redirect_to admin_homes_top_path
     else
       flash[:notice] = "注文ステータス更新に失敗しました"
       render :show
     end
-  end  
-    
+  end
+
   private
-  
+
   def order_params
     params.require(:order).permit(:customer_id, :postal_code, :address_code, :name, :postage, :bill, :payment_method, :status)
   end
+
 end
